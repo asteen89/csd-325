e9fb19d547b832c09559f3f91f8634562c13c8d9
Alisa Steensen <alisakreif@gmail.com>
2024-10-22 10:41:35 -0500

    Pushing 10/22/24

--- module-1/steensen-module1_3.py
+++ module-1/steensen-module1_3.py
@@ -1,12 +1,13 @@
 # Alisa Steensen
-# date
+# 10/22/24
 # Module 1.3
 
-
-bottles = int(input("Enter number of bottles: "))
+# Input number of bottles
+bottles = int(input("Enter number of bottles: ")) 
 
 def countdown():
-    for i in range(bottles, 1, -1):
+    """Function to countdown bottles on each loop"""
+    for i in range(bottles, 1, -1): # Start at bottles input, stop at 1 and skip by -1
         print(f"{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down and pass it around, {i - 1} bottles of beer on the wall.\n")
 
     print("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, 0 bottles of beer on the wall.\n")
