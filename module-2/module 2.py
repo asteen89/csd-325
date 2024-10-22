8db8f9514c745cdac06f2d6db00333c9ca41b8cf
Alisa Steensen <alisakreif@gmail.com>
2024-10-22 14:20:06 -0500

    Pushing 10/22/24

--- module-2/module 2.py
+++ module-2/module 2.py
@@ -9,16 +9,8 @@ def main():
     miles_input = float(input("Please input amount of miles: "))
     kilometers = miles_to_kilometers(miles_input) # Calulation for miles to kilometers
 
-    f miles_input <= 0: # another error test
-        print("Please enter a positive number.")
-        continue
-    print(f"Your miles are {miles_input} and your kilometers are {kilometers:.2f}") # Displays the requeted information
-    eturn miles_input, kilometers # Returns the requested values
+    return f"Your miles are {miles_input} and your kilometers are {kilometers:.2f}" # Displays the requeted information
 
-    print("New Test")
 
 main() # calling the main function
 
-input('Press ENTER to exit')
-
-#change
